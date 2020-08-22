01b5c3e05f1cb80551cf1ab293e84e607527eb6f
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-08-22 13:35:59 -0300

    main(dolar): Trying adding pokemon image.

--- src/Controllers/dolarToday.ts
+++ src/Controllers/dolarToday.ts
@@ -1,21 +1,27 @@
 import { TextChannel, DMChannel, NewsChannel } from "discord.js";
-import axios from 'axios';
-import puppeteer from 'puppeteer';
+import axios from "axios";
+import puppeteer from "puppeteer";
 
 async function dolarToday(channel: TextChannel | DMChannel | NewsChannel) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.google.com/search?q=dolar');
+  await page.goto("https://www.google.com/search?q=dolar");
   const dolarTodayText = await page.evaluate(() => {
-    return document.getElementsByClassName('DFlfde SwHCTb')[0].innerHTML;
+    return document.getElementsByClassName("DFlfde SwHCTb")[0].innerHTML;
   });
-  const dolarToday = Number(dolarTodayText.replace(',', '.'));
+  const dolarToday = Number(dolarTodayText.replace(",", "."));
 
-  const pokemonNumber = `${(dolarToday * 100)}`.split('.');
-  const pokemonData = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber[0]}`);
+  const pokemonNumber = `${dolarToday * 100}`.split(".");
+  const pokemonData = await axios.get(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonNumber[0]}`
+  );
   const pokemon = pokemonData.data.name;
 
   channel.send(`Dólar hoje: ${dolarToday}, Pokemon: ${pokemon}`);
+  channel.send({
+    files:
+      `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${dolarToday}.png`,
+  });
 }
 
 export default dolarToday;
